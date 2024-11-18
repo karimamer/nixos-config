81f4b952237381fd199e6ee6afd52211540ec690
karim <karim@karim.amer.sa@gmail.com>
2024-11-18 06:11:23 -0800

    sync

--- modules/casks.nix
+++ modules/casks.nix
@@ -11,5 +11,4 @@ _:
   "firefox"
   "mullvadvpn"
   "enpass"
-  "wezterm"
 ]
--- modules/default.nix
+++ modules/default.nix
@@ -24,25 +24,4 @@ in
   programs.home-manager.enable = true;
   home.stateVersion = "24.05";
 
-
-  # System Configuration for macOS Applications
-  system.activationScripts.applications.text = let
-    env = pkgs.buildEnv {
-      name = "system-applications";
-      paths = config.home.packages;
-      pathsToLink = "/Applications";
-    };
-  in
-    pkgs.lib.mkForce ''
-      # Set up applications.
-      echo "setting up /Applications..." >&2
-      rm -rf /Applications/Nix\ Apps
-      mkdir -p /Applications/Nix\ Apps
-      find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-      while read -r src; do
-        app_name=$(basename "$src")
-        echo "copying $src" >&2
-        ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
-      done
-    '';
 }
